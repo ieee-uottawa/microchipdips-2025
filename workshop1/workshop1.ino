a835684600cc8f76003ccbe3aa4a4ba4d7b4555e
Adam Dia <adambilaldia@protonmail.com>
2025-11-25 16:13:17 -0500

    custom setup and loop

--- workshop1/workshop1.ino
+++ workshop1/workshop1.ino
@@ -4,8 +4,8 @@
 #define RED_PERIOD 20
 #define GREEN_PERIOD 1000
 
-Pin_t led_r = {7, &PORTD, &DDRD, &PIND};
-Pin_t led_g = {5, &PORTD, &DDRD, &PIND};
+Pin_t led_r = { 7, &PORTD, &DDRD, &PIND };
+Pin_t led_g = { 5, &PORTD, &DDRD, &PIND };
 
 
 typedef long long int time_t;
@@ -13,31 +13,32 @@ typedef long long int time_t;
 time_t lastBlink_red = 0;
 time_t lastBlink_green = 0;
 
-
-void setup() {
-  // put your setup code here, to run once:
-
-  // set pin 7 as an output;
+void ieee_setup() {
+  // set led pins as outputs
   GPIO_PinMode(led_r, PIN_OUTPUT);
   GPIO_PinMode(led_g, PIN_OUTPUT);
-
-  //DDRD = DDRD | 0b10000000;
-
-
 }
 
-void loop() {
-
-  if(millis() - lastBlink_red > RED_PERIOD)
-  {
+void ieee_loop() {
+   if (millis() - lastBlink_red > RED_PERIOD) {
     GPIO_Toggle(led_r);
     lastBlink_red = millis();
-  }  
+  }
 
-  if(millis() - lastBlink_green > GREEN_PERIOD)
-  {
+  if (millis() - lastBlink_green > GREEN_PERIOD) {
     GPIO_Toggle(led_g);
     lastBlink_green = millis();
-  } 
-
+  }
 }
+
+int main() {
+  // setup once
+  ieee_setup();
+  // loop forever
+  for(;;)
+  {
+    ieee_loop();
+  }
+}
+
+ 
