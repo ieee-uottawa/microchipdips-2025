16a180cc00addde333c38eb5d33f97adb5c269a9
Adam Dia <adambilaldia@protonmail.com>
2025-11-28 16:17:02 -0500

    fixed code

--- workshop1/workshop1.ino
+++ workshop1/workshop1.ino
@@ -1,11 +1,11 @@
 #include <stdint.h>
 #include "gpio.h"
 
-#define RED_PERIOD 20
+#define RED_PERIOD 500
 #define GREEN_PERIOD 1000
 
-Pin_t led_r = {2, &PORTD, &DDRD, &PIND };
-Pin_t led_g = {3, &PORTD, &DDRD, &PIND };
+Pin_t led_r = {3, &PORTD, &DDRD, &PIND};
+Pin_t led_g = {2, &PORTD, &DDRD, &PIND};
 
 
 typedef long long int time_t;
@@ -13,13 +13,13 @@ typedef long long int time_t;
 time_t lastBlink_red = 0;
 time_t lastBlink_green = 0;
 
-void ieee_setup() {
+void setup() {
   // set led pins as outputs
   GPIO_PinMode(led_r, PIN_OUTPUT);
   GPIO_PinMode(led_g, PIN_OUTPUT);
 }
 
-void ieee_loop() {
+void loop() {
    if (millis() - lastBlink_red > RED_PERIOD) {
     GPIO_Toggle(led_r);
     lastBlink_red = millis();
@@ -28,16 +28,6 @@ void ieee_loop() {
   if (millis() - lastBlink_green > GREEN_PERIOD) {
     GPIO_Toggle(led_g);
     lastBlink_green = millis();
-  }
-}
-
-int main() {
-  // setup once
-  ieee_setup();
-  // loop forever
-  for(;;)
-  {
-    ieee_loop();
   }
 }
 
